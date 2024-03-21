291df79c6be356e8148d64d128f083dc29a0879c
Crowdbotics <team@crowdbotics.com>
2024-03-21 09:58:08 +0000

    Initial commit

---
+++ backend/helpdesk_new_47174/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for helpdesk_new_47174 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "helpdesk_new_47174.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "helpdesk-new-47174",
+  url: "https://helpdesk-new-47174.botics.co",
+  api: "https://helpdesk-new-47174.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
